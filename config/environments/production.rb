f0e0eb5e8c13ac6738e8c05e3e35cffe28eae729
Justin Davidson <jutstindavidson23@gmail.com>
2016-05-13 11:35:21 +1000

    Configured smtp settings in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings = {
+    address:             'smtp.mailgun.org',
+    port:                 587,
+    domain:              'example.com',
+    user_name:            ENV['MAILGUN_USERNAME'],
+    password:             ENV['MAILGUN_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
